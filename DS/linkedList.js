4fd723d153f8f92c551e67bb4be42c1b2f236648
Surya Pratap <spratap124@gmail.com>
2018-05-11 19:56:54 +0530

    Linked list implementation.

--- DS/linkedList.js
+++ DS/linkedList.js
@@ -1,3 +1,7 @@
+
+// linked list implementation
+
+
 //create new node
 function Node(val) {
     this.val = val;
@@ -10,20 +14,25 @@ function linkedList() {
     this.front = null;
     this.insert = insert;
     this.insertNode = insertNode;
+    this.deleteNode = deleteNode;
+    this.delNode = delNode;
+    this.print = print;
 }
 
+//insert a node
 function insert(data) {
     // Creating a node and initailising 
     // with data 
     var newNode = new Node(data);
 
-    // front is null then node will
+    // if front is null then node will
     // be added to the list and made front.
     if (this.front == null) {
         this.front = newNode;
     } else {
         this.insertNode(this.front, newNode);
     }
+
 }
 
 function insertNode(node, newNode) {
@@ -32,18 +41,67 @@ function insertNode(node, newNode) {
     if (node.next == null) {
         node.next = newNode;
     }
-    //if there next node then move to next node
+    //if there is next node then move to next node
     else {
         this.insertNode(node.next, newNode);
+    }
+}
+
+//delete a node
+function deleteNode(data) {
+    var head = this.front;
+    var prevNode = this.front;
+    var temp = this.front;
+    // var currentNode = this.front;
+    // var nextNode = currentNode.next;
+
+    // If head node itself holds the key to be deleted
+    if (temp != null && temp.val == data) {
+        //change head
+        head = Object.assign({}, temp.next);
+        //delete first node
+        delete temp;
+        //point the front to the head
+        this.front = head;
+    }
+
+    // Search for the key to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (temp != null && temp.val != data) {
+        prevNode = temp;
+        temp = temp.next;
+    }
+
+    // If key was not present in linked list
+    if (temp == null) {
+        console.log("data not found");
+    } else {
+        // Unlink the node from linked list
+        prevNode.next = temp.next;
+        this.print();
     }
 
 }
 
+//print the list
+function print() {
+    var pointer = this.front;
+    var list = "";
+    while (pointer !== null) {
+        list += pointer.val + "-->";
+        pointer = pointer.next;
+    }
+    console.log("List = ", list);
+    console.log("//list operations");
+    console.log("//list.insert(34)");
+    console.log("//list.deleteNode(34)");
+}
 
-//initialize the list
+
 var list = new linkedList();
-
-list.insert(2);
+list.insert(3);
+list.insert(4);
 list.insert(5);
-
-console.log(list.front);
+list.insert(6);
+list.insert(8);
+list.print();
